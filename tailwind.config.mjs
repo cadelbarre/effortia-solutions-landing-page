81a71c83a79172bbb5abb99e7b1279f77542be7f
Carlos Delbarre <cadelbarre@gmail.com>
2024-02-27 17:55:02 -0500

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#ED7D3A'
+			}
+		},
+	},
+	plugins: [],
+}
