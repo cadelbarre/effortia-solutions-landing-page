cfe93434df0cbbf85f8cdd3d0808f94492cce6db
Carlos Delbarre <cadelbarre@gmail.com>
2024-11-12 16:40:56 -0500

    feat: install react

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,15 @@
-import animate from '@midudev/tailwind-animations'
+import animate from "@midudev/tailwind-animations";
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				primary: '#FF5800'
-			}
-		},
-	},
-	plugins: [animate],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF5800",
+        primarydark: "#f34e00",
+      },
+    },
+  },
+  plugins: [animate],
+};
 //ED7D3A
