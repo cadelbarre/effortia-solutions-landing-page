b46cf4b4e604a5a32d1a082655752e979fd055a4
Carlos Delbarre <cadelbarre@gmail.com>
2024-11-13 19:14:32 -0500

    feat: add env variable

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,26 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig, envField } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  experimental: {
+    env: {
+      schema: {
+        PUBLIC_SERVICES_EMAIL: envField.string({
+          context: "client",
+          access: "public",
+        }),
+        PUBLIC_TEMPLATE_EMAIL: envField.string({
+          context: "client",
+          access: "public",
+        }),
+        PUBLIC_USERKEY_EMAIL: envField.string({
+          context: "client",
+          access: "public",
+        }),
+      },
+    },
+  },
+  integrations: [tailwind(), react()],
 });
