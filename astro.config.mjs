dfe30df2667cfeee8080d80b602d64a417c4877c
Carlos Delbarre <cadelbarre@gmail.com>
2024-11-15 17:35:02 -0500

    feat: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,23 @@ import { defineConfig, envField } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+
   experimental: {
     env: {
       schema: {
-        PUBLIC_SERVICES_EMAIL: envField.string({
-          context: "client",
-          access: "public",
-        }),
-        PUBLIC_TEMPLATE_EMAIL: envField.string({
-          context: "client",
-          access: "public",
-        }),
-        PUBLIC_USERKEY_EMAIL: envField.string({
-          context: "client",
+        RESEND_API_KEY: envField.string({
+          context: "server",
           access: "public",
         }),
       },
     },
   },
+
   integrations: [tailwind(), react()],
+  adapter: vercel(),
 });
